dfc3f570a7561fafb288ed0a74f3ab8ac5fa919f
Carla Lourens <carlalourens@icloud.com>
2025-07-01 18:26:45 +0200

    Commit 4 - Added Error 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import RSVPView from '@/views/RSVPView.vue'
-
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   { path: '/', name: 'HomeView', component: HomeView, meta: { title: 'Home Page' } },
-  { path: '/rsvp', name: 'RSVPView', component: RSVPView, meta: { title: 'RSVP Page' } }
+  { path: '/rsvp', name: 'RSVPView', component: RSVPView, meta: { title: 'RSVP Page' } },
+  { path: '/:catchAll(.*)', name: 'NotFound', component: NotFound }
 ];
 
 export default createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page not-found">
+    <h1>404 - Page Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+  color: gray;
+  font-family: Arial, sans-serif;
+}
+</style>
